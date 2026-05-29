540e7e34df468936451d56e7aee960572cd263c9
JAVO551 <javier.diazrivera551@gmail.com>
2026-05-29 16:56:44 -0600

    Codigos con comentarios para los ejercicios

--- ros2_ws/src/hardware/mobile_base/mobile_base/pwm_movement.py
+++ ros2_ws/src/hardware/mobile_base/mobile_base/pwm_movement.py
@@ -1,10 +1,16 @@
+#
+# ROVER LUNARES PARA EL APRENDIZAJE - ENP6-UNAM, 2026
+# MOVIMIENTO DEL ROVER CON PWM
+#
+#El siguieunte código implementa un nodo de ROS2 que permite controlar el movimiento del rover utilizando mensajes de tipo Twist para publicar velocidades lineales y angulares. 
+#El nodo incluye funciones para mover el rover hacia adelante y para girar en su propio eje, calculando el tiempo necesario para realizar cada movimiento basado en la velocidad deseada y la distancia o ángulo a recorrer. 
+#Además, se incluye una función para detener el rover después de completar cada movimiento.
+#Revisar la linea 36 del código y seguir sus instrucciones.
+
+
 import rclpy
 from rclpy.node import Node
-from tf2_ros import Buffer, TransformListener
 from geometry_msgs.msg import Twist
-from nav_msgs.msg import Path
-from rclpy.duration import Duration
-from std_msgs.msg import Bool
 import time
 import math
 
@@ -16,21 +22,31 @@ class ObjectiveMovementPWM(Node):
 
 
         self.publisher_vel = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.move_time = 0.0
-        self.rover_speed = 0.5 # 1 m/s is 127 pwm in mobile base
+
+        self.move_time = 0.0 #Calculo de la cantidad de tiempo que se debe mover el rover
+        self.rover_speed = 0.5 # Velocidad de desplazamiento para el rover en m/s o rad/s dependiendo de la función
+        self.radius_turn = 3.4 # radio físico del rover para un giro en su propio eje
+
         self.time_resolution = 0.1
-        self.calibration = 1.5 #1.7
-        self.radius_turn = 3.4 # radius for turning in m
+        self.calibration = 1.5 
+        
         
         self.get_logger().info("Nodo de distancia objectivo iniciado")
 
-        #self.move_forward(1.0)
-        self.move_turn(1.57)           #Equivale 90| a Izq
-        # self.move_forward(1.0)
-        # self.move_forward(1.0)
+        #Acontinuación implemente la secuencia de funciones correctas para mover el rover con el fin de describir un cuadrado de 1 metro de lado.
+        #Las funciones deben estar escritas al terminar estos comentarios con el mismo identado.
+        #El cuadraddo se puede implementar con con 4 giros de 90 grados y 4 movimientos hacia adelante. 
+        
+        #La función move_foward se encarga de mover el rover segun una distancia dada en metros.
+        #La función move_turn se encarga de girar el rover en su propio eje segun el un ángulo dado en radianes.
+
+        #self.move_forward(0.0)
+        #self.move_turn(0.0)           
+        
         
 
-    def move_forward (self, distance):
+    
+    def move_forward (self, distance): #Función para mover el rover hacia adelante o atras.
             msg = Twist()    
             self.move_time = (abs(distance) / self.rover_speed)
             
@@ -43,17 +59,17 @@ class ObjectiveMovementPWM(Node):
 
             msg.linear.x = self.rover_speed
             msg.angular.z = 0.0
-            
 
             for i in range(int(self.pulses)):
                 self.publisher_vel.publish(msg)
                 time.sleep(self.time_resolution*self.calibration)
             self.robot_stop()
-
-    def move_turn (self, angle): 
+    
+    def move_turn (self, angle): #Función para girar el rover en su propio eje.
                 msg = Twist() 
                 self.move_time = (abs(angle) / self.rover_speed) 
                 self.pulses= self.move_time/self.time_resolution 
+                self.get_logger().info(f"Tiempo de movimiento: {self.move_time}")
                 
                 
                 if angle < 0:
@@ -67,11 +83,11 @@ class ObjectiveMovementPWM(Node):
 
                 for i in range(int(self.pulses)):
                     self.publisher_vel.publish(msg)
-                    time.sleep(self.time_resolution*self.calibration)
+                    time.sleep(self.time_resolution)
                 self.robot_stop()
 
 
-    def robot_stop(self):
+    def robot_stop(self): #Función para detener cualquier movimiento del rover.
         msg = Twist() 
         msg.linear.x = 0.0
         msg.angular.z = 0.0
--- ros2_ws/src/hardware/mobile_base/mobile_base/servo_turn.py
+++ ros2_ws/src/hardware/mobile_base/mobile_base/servo_turn.py
@@ -1,3 +1,12 @@
+#
+# ROVER LUNARES PARA EL APRENDIZAJE - ENP6-UNAM, 2026
+# CONTROL DE SERVOS PARA EL ROVER
+#
+#El siguieunte código implementa un nodo de ROS2 que permite controlar los servos del rover utilizando mensajes de tipo Twist para publicar velocidades lineales y angulares.
+#El código incluye las funciones necesarias para trabajar con los servo motores Dynamixel MX-64, utilizando su SDK para enviar comandos y leer información de posición.
+#Se implentan 2 funciones que permiten calcular el ángulo deseado y una segunda para imprimir el ángulo real del servomotor.
+#Revisar la linea 56 del código y seguir sus instrucciones.
+
 from dynamixel_sdk import *
 import math
 import rclpy
@@ -7,16 +16,11 @@ import time
 
 ADDR_TORQUE_ENABLE = 24
 ADDR_GOAL_POSITION = 30
-LEN_GOAL_POSITION = 2 #Data lenght for position
+LEN_GOAL_POSITION = 2 
 ADDR_PRESENT_POSITION = 36
-
-
-
 PROTOCOL_VERSION = 1.0  
-
 BAUDRATE = 57600  
 DEVICE_NAME = '/dev/ttyUSB0'  
-
 TORQUE_ENABLE = 1 
 TORQUE_DISABLE = 0  
 NUM_SERVOS = 4
@@ -26,9 +30,9 @@ class ServosNode(Node):
     def __init__(self):
         super().__init__('servo_node')
 
-        self.goal_position = [2172,2048,2048,2048] #Initial position for each dynamixel 
-        self.prev_goal_position = [2048,2048,2048,2048] #Previous position for each dynamixel, used for odometry calculations
-        self.DXL_ID = [1,3,4,2] #ID for all 4 motors #1&3 = left wheels, 4&2 = right wheels
+        self.goal_position = [2172,2048,2048,2048] 
+        self.prev_goal_position = [2048,2048,2048,2048] 
+        self.DXL_ID = [1,3,4,2] 
 
         self.port_handler = PortHandler(DEVICE_NAME)
         self.packet_handler = PacketHandler(PROTOCOL_VERSION)
@@ -38,19 +42,27 @@ class ServosNode(Node):
 
         self.diameter = 0.107
         self.radius = self.diameter / 2.0
-        self.width = 0.34 #Rover measure of left wheels to right wheels
-        self.height = 0.31 #Rover measure of center wheels to front wheels
-        self.ppr = 4400
+        self.width = 0.34 
+        self.height = 0.31 
+        self.ppr = 4450
        
-        self.meters_per_tick = (math.pi * self.diameter) / self.ppr #For encoders
+        self.meters_per_tick = (math.pi * self.diameter) / self.ppr 
         
-        #Initial configurartion of dynamixels
+        #Posición y configuración inicial de los servos.
         self.setup_dynamixel(self.DXL_ID)
 
+        time.sleep(2)
 
+        #1- Revise la linea 142 del código, se encuentra en la función move_servos, realice el ejercicio propuesto para completar el código.
+
+        #2- Modifique la llamada a la función self.move_servos para imprimir el ángulo real de cada servo.
+        #   La función move_servos recibe como primer argumento la velocidad lineal en m/s y como segundo argumento la velocidad angular en rad/s.
+        #   Revise la información impresa en la terminal y compare los ángulos reales con los calculados en la clase, revise si la orientación es correcta.
 
 
         self.move_servos(1.0,1.0)
+
+
 
         
 
@@ -58,12 +70,11 @@ class ServosNode(Node):
 
 
     def move_servos(self,linear, angular):
-        if angular > 0.005 or angular < -0.005: #Poner un umbral
-            
+
+        if angular > 0.005 or angular < -0.005: 
             wheel_information = self.get_wheel_configuration (linear,angular)   
 
         else :
-
             wheel_information = [[linear,linear,linear,linear,linear,linear],[0,0,0,0,0,0]]
 
 
@@ -77,9 +88,9 @@ class ServosNode(Node):
 
         for c in range(NUM_SERVOS):
                 param = [DXL_LOBYTE(int(round(self.goal_position[c]))),DXL_HIBYTE(int(round(self.goal_position[c])))]
-                dxl_addparam_result = self.groupSyncWrite.addParam(self.DXL_ID[c], param)
-                
-            
+                dxl_addparam_result = self.groupSyncWrite.addParam(self.DXL_ID[c], param)   
+
+        #Commando para mover los servomotores a la posición deseada.  
         try:
             dxl_comm_result = self.groupSyncWrite.txPacket()
         except Exception as e:
@@ -91,25 +102,29 @@ class ServosNode(Node):
         for c in range(NUM_SERVOS):
            
             time.sleep(0.5)
+
+            #Se imprime el valor que el servomotor lee como posición actual, se convierte a grados para una mejor interpretación.
             posicion, result, error = self.packet_handler.read2ByteTxRx(self.port_handler,self.DXL_ID[c],ADDR_PRESENT_POSITION)
-            if result == COMM_SUCCESS: #COMM_SUCCES ESTA DEFINIDO DENTRO DEL SDK, SU VALOR ES DE 0
+            if result == COMM_SUCCESS:
                 posicion = (posicion - 2048) * (360/4096)
                 self.get_logger().info(f"Posición actual del servo {self.DXL_ID[c]}: {posicion}")
-
-
-
         self.port_handler.closePort()
 
 
 
-    def get_wheel_configuration (self,linear,angular): #FUNCTION TO OBTAIN WHEEL INFORMATION
+
+    #Cálculos matemáticos para la el ángulo deseado de cada servo
+    def get_wheel_configuration (self,linear,angular): 
         
+        #Cálculo del radio de giro del rover, se utiliza la relación entre la velocidad lineal y angular para obtener el radio de giro.
         radius = linear/angular
             
+        #Distancia del centro del círculo a cada lateral del rover.
         radius_left_center = radius - (self.width/2)
         radius_right_center = radius + (self.width/2)
 
-        if linear > 0.005 or linear < -0.005: #if linear!=0
+        #Los siguientes cálculo se utilizan para corregir errores en la lóica de cálculo al programase en código
+        if linear > 0.005 or linear < -0.005: 
             sign = radius/abs(radius)
             sign2 = linear/abs(linear)
             sign3 = 1
@@ -121,42 +136,49 @@ class ServosNode(Node):
             sign3 = (angular/abs(angular))
             sign4 = sign3
             
-
+        #Cálculo de la distancia del centro del círculo a cada rueda frontal, se utiliza el teorema de Pitágoras para obtener esta distancia.
         radius_left_frontal = math.sqrt(self.height**2+radius_left_center**2) * (sign)
         radius_right_frontal = math.sqrt(self.height**2+radius_right_center**2) * (sign)
 
-        #Wheel angles
-        angle_left_front = math.atan2(self.height,  radius_left_center* (sign)) * (sign)#Radians
-        angle_left_rear = math.atan2(-self.height,radius_left_center* (sign))* (sign)
-        angle_right_front = math.atan2(self.height,radius_right_center* (sign)) * (sign)
-        angle_right_rear = math.atan2(-self.height,radius_right_center* (sign))* (sign)
+        #Cálculo de los ángulos deseados para cada servo.
+        #Descomente las la cuarteta de lineas correctas para calcular los ángulos deaseados.
+        #Las opciones a) y b) solo se diferencian en la función trigonométrica utilizada.
+
+        #a)
+        # angle_left_front = math.atan2(self.height,  radius_left_center* (sign)) * (sign)
+        # angle_left_rear = math.atan2(-self.height,radius_left_center* (sign))* (sign)
+        # angle_right_front = math.atan2(self.height,radius_right_center* (sign)) * (sign)
+        # angle_right_rear = math.atan2(-self.height,radius_right_center* (sign))* (sign)
+
+        #b)
+        # angle_left_front = math.cos(self.height,  radius_left_center* (sign)) * (sign)
+        # angle_left_rear = math.cos(-self.height,radius_left_center* (sign))* (sign)
+        # angle_right_front = math.cos(self.height,radius_right_center* (sign)) * (sign)
+        # angle_right_rear = math.cos(-self.height,radius_right_center* (sign))* (sign)
         
 
-        #Wheel speeds:
+        #Velocidad de cada rueda (no se usa esta información en este código):
         v_lf = abs(radius_left_frontal * angular) * sign2
-        v_lc = abs((radius_left_center-0.03) * angular) * sign2 * -sign4
+        v_lc = abs((radius_left_center) * angular) * sign2 * -sign4
         v_lr = abs(radius_left_frontal * angular)* sign2
         v_rf = abs(radius_right_frontal * angular)* sign2
-        v_rc = abs((radius_right_center+0.03) * angular)* sign2 * sign3
+        v_rc = abs((radius_right_center) * angular)* sign2 * sign3
         v_rr = abs(radius_right_frontal * angular)* sign2
 
         return [[v_lf,v_lc,v_lr,v_rf,v_rc,v_rr],[angle_left_front,0,angle_left_rear,angle_right_front,0,angle_right_rear]]
+    
 
-    def radian_to_dynamixel (self,angles): #Angles in radian to angles in bits for each dynamixel
+    #Función para convertir los ángulos calculados a valores que el Dynamixel pueda interpretar
+    def radian_to_dynamixel (self,angles): 
 
         angles[0] = 2172 - (4096/(2*math.pi))* angles[0]
         angles[2] = 2048 - (4096/(2*math.pi))* angles[2]
         angles[3] = 2048 - (4096/(2*math.pi))* angles[3]
-        angles[5] = 2048 - (4096/(2*math.pi))* angles[5] #2048
+        angles[5] = 2048 - (4096/(2*math.pi))* angles[5] 
 
         return angles
 
-        
-
-
-
-
-
+    #Función para la configuración del los servos al iniciar el código.
     def setup_dynamixel(self, dxl_id):
         
         for c in range(NUM_SERVOS):
@@ -182,8 +204,6 @@ class ServosNode(Node):
 
     
     def __del__(self):
-        
-        #Disable movement in dynamixel servos
         for c in range(NUM_SERVOS):
             self.packet_handler.write1ByteTxOnly(self.port_handler,
                                            self.DXL_ID[c],
